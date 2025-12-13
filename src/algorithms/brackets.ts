1f47afc5168a538e255e052afaa1b0b534b97424
Patrik <patrikludvik@gmail.com>
2025-12-13 06:46:18 +0100

    brackets alg

---
+++ src/algorithms/brackets.ts
@@ -0,0 +1,39 @@
+export function areBracketsBalanced(expression: string): boolean {
+	const stack: string[] = [];
+
+	for (const char of expression) {
+		if (char === "(") {
+			stack.push(char);
+		} else if (char === ")") {
+			if (stack.length === 0) {
+				return false;
+			}
+			stack.pop();
+		}
+	}
+
+	return stack.length === 0;
+}
+
+/*
+pseudokód:
+
+prijmi string
+vytvor prazdny zasobnik
+
+pro kazdy znak ve stringu
+  pokud znak je "("
+    vloz ho do zasobniku
+  pokud znak je ")"
+    pokud je zasobnik prazdny
+      vrat false
+    jinak
+      odeber prvek ze zasobniku
+
+pokud je zasobnik prazdny
+  vrat true
+jinak
+  vrat false
+
+
+*/
--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,9 +1,17 @@
 import { bubbleSort } from "../algorithms/bubbleSort";
+import { areBracketsBalanced } from "../algorithms/brackets";
 
-const data = [5, 3, 8, 4, 2];
+// const data = [5, 3, 8, 4, 2];
+
+// console.log("Vstup:", data);
+
+// const sorted = bubbleSort(data);
+
+// console.log("Výstup:", sorted);
+
+const data = "(ddasdasdasd(()dasd)";
 
 console.log("Vstup:", data);
 
-const sorted = bubbleSort(data);
-
-console.log("Výstup:", sorted);
+const brackets = areBracketsBalanced(data);
+console.log("Výstup:", brackets);
