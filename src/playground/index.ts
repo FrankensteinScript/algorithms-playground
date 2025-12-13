68a5f6619c35cb846e2c7b1436571c9374cb7182
Patrik <patrikludvik@gmail.com>
2025-12-13 05:57:27 +0100

    bubble

---
+++ src/playground/index.ts
@@ -0,0 +1,9 @@
+import { bubbleSort } from "../algorithms/bubbleSort";
+
+const data = [5, 3, 8, 4, 2];
+
+console.log("Vstup:", data);
+
+const sorted = bubbleSort(data);
+
+console.log("Výstup:", sorted);
